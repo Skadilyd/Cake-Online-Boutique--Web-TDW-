bde6e4eb9dc1a64c9ba9f47953732e01d73a4916
Lydia <167013866+Skadilyd@users.noreply.github.com>
2026-05-28 20:40:45 +0200

    Add bestcake.php with video and header inclusion

---
+++ public_html/bestcake.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/> 
+        <title> Cake(?) </title> 
+        <link rel="stylesheet" type="text/css" href="css/maincss.css">
+        <link rel="icon" type="image/x-icon" href="/img/Logo.jpg">
+    </head>
+    <body>
+        <?php include_once __DIR__ . "/controladors/headerPagina.php"; ?>
+        <video width="640" height="480" controls>
+            <source src="vid/bestcake.mp4" type="video/mp4">
+        </video>
+    </body>
+</html>
